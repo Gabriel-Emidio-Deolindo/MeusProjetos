80192dd34fdd5a6780d45bdca68431f45c6a46cd
GabrielEmídioDeolindo <gabriel.emidiolindo@gmail.com>
2026-08-28 01:02:00 -0300

    Implementando PWA

--- calculadora/sw.js
+++ calculadora/sw.js
@@ -35,8 +35,8 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then(
-      (cachedResponse) => cachedResponse || fetch(event.request),
-    ),
+    caches
+      .match(event.request)
+      .then((cachedResponse) => cachedResponse || fetch(event.request)),
   );
 });
